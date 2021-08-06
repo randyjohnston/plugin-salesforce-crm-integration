3d9a44d33e297bc989ee348b184c70a592c9a2b2
Randy Johnston <rjohnston@twilio.com>
2021-08-06 14:59:09 -0500

    Refactor to remove duplicate code

--- serverless/functions/get-access-token.js
+++ serverless/functions/get-access-token.js
@@ -22,33 +22,25 @@ exports.handler = async function (context, event, callback) {
   const connection = new jsforce.Connection({ oauth2: oauth2 });
 
   // Exchange authorization code for access token & refresh token; check for valid SFDC Org. ID
-  await connection.authorize(event.code, function (err, userInfo) {
-    if (err) {
-      console.log('User unauthorized or OAuth error');
-      response.setBody('Authorization failed');
-      response.setStatusCode(403);
-      return callback(null, response);
-    } else if (userInfo.organizationId !== context.SFDC_ORG_ID) {
+  try {
+    const userInfo = await connection.authorize(event.code);
+    if (userInfo.organizationId !== context.SFDC_ORG_ID) {
       console.log('User from unauthorized org. ', userInfo.organizationId);
       response.setBody('Authorization failed');
       response.setStatusCode(403);
       return callback(null, response);
     }
     console.log('User authorized from org. ', userInfo.organizationId);
-  });
-
-  // Get user identity for access token
-  const identityInfo = await connection.identity(function (err) {
-    if (err) {
-      response.setBody('Authorization failed');
-      response.setStatusCode(403);
-      return callback(null, response);
-    }
-  });
-
-  await refreshToken.refreshToken(twilioClient, context, connection, response);
-
-  response.setBody(`Successfully linked Flex to Salesforce for ${identityInfo.username}. Please close this window.`);
-  return callback(null, response);
+    // Get user identity for access token
+    const identityInfo = await connection.identity();
+    await refreshToken.refreshToken(twilioClient, context, connection, response);
+    response.setBody(`Successfully linked Flex to Salesforce for ${identityInfo.username}. Please close this window.`);
+    return callback(null, response);
+  } catch (err) {
+    console.error(err);
+    response.setBody('Authorization failed');
+    response.setStatusCode(403);
+    return callback(null, response);
+  }
 
 };
